68147568199953d9ef86daee4b0cee8cf4c901cc
LaughingThroll <57355241+LaughingThroll@users.noreply.github.com>
2020-10-20 22:35:16 +0300

    added reviews (#5)

--- gulp-tasks/tasks/StyleLibs.js
+++ gulp-tasks/tasks/StyleLibs.js
@@ -3,7 +3,8 @@ module.exports = () => {
   G.gulp.task('styleLibs', function () {
     return G.gulp.src([
       'node_modules/normalize.css/normalize.css',
-      'node_modules/slick-carousel/slick/slick.css'
+      'node_modules/slick-carousel/slick/slick.css',
+      'node_modules/ion-rangeslider/css/ion.rangeSlider.css'
   
     ])
       .pipe(G.concat('libs.min.css'))
